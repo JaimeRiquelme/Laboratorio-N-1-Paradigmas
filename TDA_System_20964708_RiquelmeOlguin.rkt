2ed9a5bf5a86fc445947141c333ad07a03e03056
Jaime Riquelme <jaime.riquelme@usach.cl>
2023-03-29 22:54:15 -0300

    Cambios estructura

--- TDA_System_20964708_RiquelmeOlguin.rkt
+++ TDA_System_20964708_RiquelmeOlguin.rkt
@@ -2,103 +2,74 @@
 (require racket/date)
 
 
-; Dominio: System (String)
-; Recorrido: New System
-; Descripción: Función que crea un nuevo sistema
+;Contructor system
+(define (make-system nombre drive usuarios ruta fecha-actual)
+  (list nombre drive usuarios ruta fecha-actual))
+
 (define (system nombre)
-  (make-system nombre null null (current-date)))
+  (make-system nombre null null null (fecha-actual)))
 
+(define get-nombre (lambda (system) (car system)))
 
+(define get-drive (lambda (system) (cadr system)))
 
-(define make-user(lambda (name)(list name)))
+(define get-usuarios (lambda (system) (caddr system)))
 
+(define get-ruta (lambda(system)(cadr(reverse system))))
 
-(define (make-system name drive usuarios fecha-creacion)
-  (list name drive usuarios fecha-creacion))
+(define get-fecha (lambda (system) (car (reverse system))))
+
+(define set-nombre-system (lambda (system-nombre nombre)
+                            (make-system nombre (get-drive system-nombre)(get-usuarios system-nombre)
+                                         (get-ruta system-nombre)(get-fecha system-nombre))))
+(define set-drive-system(lambda (system drive)
+                          (make-system (get-nombre system) drive
+                                       (get-usuarios system) (get-ruta system) (get-fecha system))))
+
+(define set-usuarios-system(lambda (system usuarios)(make-system
+                                       (get-nombre system) (get-drive system) usuarios (get-ruta system) (get-fecha system))))
+
+(define set-ruta-system(lambda (system ruta)(make-system (get-nombre system)
+                                        (get-drive system) (get-usuarios system) ruta (get-fecha system))))
+
+(define set-fecha-system(lambda (system fecha)(make-system (get-nombre system)
+                                        (get-drive system) (get-usuarios system) (get-ruta system) fecha)))
+
+(define (fecha-actual)(define fecha (current-date))
+        (list (date-day fecha)
+               (date-month fecha)
+               (date-year fecha)
+               (string-append (number->string (date-hour fecha))
+                       ":"
+                (number->string (date-minute fecha)))))
 
 
 ;RUN
-(define (run sistema comando)(comando sistema))
+;Dominio: Comando
+;recorrido: Run comando
+;Descripcion: Funcion que ejecuta comandos enviados al sistema.
+(define run(lambda (system comando)(comando system)))
 
 
-;;CAPA SELECTORA DEL SYSTEM:
-(define get-name car)
-(define get-drive cadr)
-(define get-usuarios caddr)
-(define (get-fecha system)(car (reverse system)))
-
-
-;CAPA MODIFICADORA DEL DRIVE:
+;DRIVE Contructor
 (define (make-drive letra nombre capacidad)
   (list letra nombre capacidad))
 
-(define (login-list name-user)
-                    (list name-user))
-(define (get-login-list system)
-  (login-list system))
 
-
-(define set-login-list
-  (lambda (system login-list)
-    (make-system (get-name system)
-                 (get-drive system)
-                 (get-usuarios system)
-                 (current-date)
-                 (if (null? login-list) '() login-list))))
-
-
-
-
-
+;RUN
+;Dominio: System
+;recorrido: Sytem con un nuevo drive
+;Descripcion: Funcion que ejecuta comandos enviados al sistema.
 (define add-drive
   (lambda (system)
     (lambda (letra nombre capacidad)
       (if (not (member letra (map car (get-drive system))))
-          (make-system (get-name system)
+          (make-system (get-nombre system)
                        (cons (make-drive letra nombre capacidad) (get-drive system))
                        (get-usuarios system)
-                       (current-date))
+                       (get-ruta system)
+                       (get-fecha system))
           system))))
-
-
-
-
-(define add-user
-  (lambda (system)
-    (lambda (name-user)
-      (if (member name-user (map car (get-usuarios system)))
-          (begin
-            (display "Usuario ya existe: ")
-            (display name-user)
-            (display "\n")
-            system)
-          (make-system (get-name system)
-                       (get-drive system)
-                       (append (get-usuarios system) (list (make-user name-user)))
-                       (current-date))))))
-
-
-
-(define login
-  (lambda (system)
-    (lambda (name-user)
-      (if (member name-user (map car(get-usuarios system)))
-          (if (null? (get-login-list system))
-              (begin
-                (run system (set-login-list (get-login-list system) (login-list name-user)))
-                (display "Bienvenido, ")
-                (display name-user)
-                (display "\n"))
-              (display "Ya hay un usuario conectado\n"))
-          (display "Usuario no existe\n")))))
-
-(define logout
-  (lambda (system)
-    (run system (set-login-list '()))))
-
-
-
-
 
 
 
@@ -110,19 +81,6 @@
 (define S1 ((run S0 add-drive) #\C "SO" 1000))
 (define S2 ((run S1 add-drive) #\C "SO1" 3000))
 (define S3 ((run S2 add-drive) #\D "Util" 2000))
-
-S3
-
-;añadiendo usuarios. Incluye caso S6 que intenta registrar usuario duplicado
-(define S4 ((run S3 add-user) "user1"))
-(define S5 ((run S4 add-user) "user1"))
-(define S6 ((run S5 add-user) "user2"))
-
-;iniciando sesión con usuarios. Incluye caso S8 que intenta iniciar sesión con user2 sin antes haber salido con user1
-(define S7 ((run S6 login) "user1"))
-(define S8 ((run S7 login) "user2"))
-
-
 
 
 
